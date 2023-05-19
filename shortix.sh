ab4f533c2b5a6b6fce655acc2051f23ab4b29d64
Jan Schröder <jannomag@gmx.de>
2023-05-19 10:40:06 +0200

    Create shortix.sh
    Created the script

---
+++ shortix.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#Set variables for all needed files an paths
+PROTONTRICKS="flatpak run com.github.Matoking.protontricks"
+SHORTIX_DIR=/home/deck/Shortix
+TEMPFILE=/tmp/shortix
+COMPDATA=/home/deck/.steam/steam/steamapps/compdata
+
+TIME=15
+
+#Run the script if there's at least one directory newer than TIME variable (minutes).
+if [ $(find $COMPDATA -mmin -$TIME -type d) ]; then
+    #Create Shortix directory if not already present
+    mkdir -p $SHORTIX_DIR
+
+    #Run protontricks to list all installed games and write the result into the temp file
+    eval "$PROTONTRICKS" -l > $TEMPFILE
+
+    #remove all lines which doesn't have a round bracket in it
+    sed -i -ne '/)/p' $TEMPFILE
+
+    #Remove the "Non_Steam shortcut: " string from temp file
+    sed -i 's/Non-Steam shortcut: //' $TEMPFILE
+
+    #Replace the opening round bracket with a simicolon in temp file - this is needed to reading the contents as variable later on
+    sed -i 's/ (/;/' $TEMPFILE
+
+    #Do the same with the closing round bracket
+    sed -i 's/)/;/' $TEMPFILE
+
+    #Remove non existant symlinks
+    find -L $SHORTIX_DIR -maxdepth 1 -type l -delete
+
+    #Create symlinks based on the data from the temp file.
+    #IFS defines the semicolon as column separator
+    #Then read the both columns as variables and create symlinks based on the data of each line
+    while IFS=';' read game_name prefix_id
+    do
+        ln -sf "$COMPDATA/$prefix_id" "$SHORTIX_DIR/$game_name"
+    done < $TEMPFILE
+fi
