3c6b4156343dfaa471556d787f12e122602bebda
Jan Schröder <jannomag@gmx.de>
2023-05-19 12:44:49 +0200

    Update shortix_install.sh

--- shortix_install.sh
+++ shortix_install.sh
@@ -1,7 +1,12 @@
 #/bin/bash
+echo "##########################"
+echo "#   INSTALLING SHORTIX   #"
+echo "##########################"
 mkdir -p /home/deck/Shortix
 cp /tmp/shortix/shortix.sh /home/deck/Shortix
+chmod +x /home/deck/Shortix/shortix.sh
 cp /tmp/shortix/shortix.service /home/deck/.config/systemd/user
 systemctl --user daemon-reload
 systemctl --user enable shortix.service
 systemctl --user start shortix.service
+echo  "DONE!"
