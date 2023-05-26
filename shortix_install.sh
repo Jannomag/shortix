2c0f758b2b88849f6b7a4a927d15126d9f2a57f6
Jan Schröder <jannomag@gmx.de>
2023-05-26 12:43:26 +0200

    Update shortix_install.sh
    Added the feature to append the prefix id to the shortcut names

--- shortix_install.sh
+++ shortix_install.sh
@@ -16,6 +16,13 @@ cp /tmp/shortix/shortix.sh $HOME/Shortix
 cp /tmp/shortix/remove_prefix.sh $HOME/Shortix
 chmod +x $HOME/Shortix/shortix.sh
 
+read -e -n 1 -p "Would you like to add the prefix id to the shortcut name (Like 'Game Name (123455678)')? [Y/n]: "
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  if [ ! -f $HOME/Shortix/.id ]; then
+    touch $HOME/Shortix/.id
+  fi
+fi   
+
 read -e -n 1 -p "Would you like to setup system service for background updates [Y/n]: "
 if [[ ! $REPLY =~ ^[Nn]$ ]]; then
   if [ ! -d $HOME/.config/systemd/user ]; then
